ddb0eb0bcb125f3150b342ea445c6ac97093c46a
nicfreeman1209 <nicfreeman1209@gmail.com>
2020-09-22 11:38:47 +0100

    switch to python script for html compile

---
+++ MAS350/_html_to_subfolder.py
@@ -0,0 +1,27 @@
+# runs lwarpmk, moves some files and copies others
+# end result: 
+#   complete website ends up in ./out_html/, whilst keeping original tex working dir moderately clean
+
+from glob import glob
+import os
+import shutil
+
+if os.path.exists("./out_html"):
+	shutil.rmtree("./out_html/")
+os.makedirs("out_html")
+	
+os.system("lwarpmk html1")
+
+move_extensions = ["html"]
+copy_extensions = ["png", "jpg", "txt", "css"]
+
+for ext in move_extensions:
+	files = glob("./*." + ext)
+	for file in files:
+		shutil.move(file, os.path.join("./out_html", file))
+		
+for ext in copy_extensions:
+	files = glob("./*." + ext)
+	for file in files:
+		shutil.copy(file, os.path.join("./out_html", file))
+		
